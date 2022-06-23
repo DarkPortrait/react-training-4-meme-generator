fbe33a804d864c912a2c28889612b16e8e7da2ea
Tanvir Alam <8191415+DarkPortrait@users.noreply.github.com>
2022-06-23 19:48:03 -0400

    Add form inputs and buttons

--- src/components/MemeGen.js
+++ src/components/MemeGen.js
@@ -1,6 +1,22 @@
 export default function MemeGen(){
   return(
-    <p>MemeGen</p>
+    <main>
+      <div className='form'>
+        <input 
+          type='text'
+          placeHolder='Top text'
+          className='form--input'
+        />
+        <input 
+          type='text'
+          placeHolder='Bottom text'
+          className='form--input'
+        />
+        <button className='form--button'>
+          Get a new meme image 🖼
+        </button>
+       </div>
+     </main>
   )
 }
 
