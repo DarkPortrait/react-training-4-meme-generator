af3aadcace885479d5f680443c535cd9929e6fe0
Tanvir Alam <8191415+DarkPortrait@users.noreply.github.com>
2022-06-23 20:45:10 -0400

    Changing images

--- src/components/MemeGen.js
+++ src/components/MemeGen.js
@@ -1,4 +1,19 @@
+import React from 'react'
+import data from '../data.js'
+
 export default function MemeGen(){
+
+  var firstMeme='https://i.imgflip.com/1bgw.jpg'
+
+  const [meme, setMeme] = React.useState(firstMeme)
+
+  function clickHandler(){
+    const memesData = data.data.memes
+    const randomNumber = Math.floor(Math.random()*memesData.length)
+    console.log(memesData[randomNumber].url)
+    setMeme(memesData[randomNumber].url)
+  }
+
   return(
     <main>
       <div className='form'>
@@ -12,9 +27,16 @@ export default function MemeGen(){
           placeHolder='Bottom text'
           className='form--input'
         />
-        <button className='form--button'>
+        <button 
+          className='form--button'
+          onClick={clickHandler}
+        >
           Get a new meme image 🖼
         </button>
+        <img
+          src={meme}
+          className='form--img'
+        />
        </div>
      </main>
   )
--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ import ReactDOM from 'react-dom'
 import './style.css'
 import App from './App'
 
-console.log("hi mihi")
-
 ReactDOM.render(
   <App />,
   document.getElementById('root')
